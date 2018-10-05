b27104c67f38d08b0a22108f2c51cf5bc89795fa
glasan54 <pakapol.s@ku.th>
2018-10-05 14:17:36 +0700

    unfinish

--- src/main/java/adapterPattern/DuckSimulator.java
+++ src/main/java/adapterPattern/DuckSimulator.java
@@ -1,5 +1,7 @@
 package adapterPattern;
 
+import decoratorPattern.QuackCounter;
+
 public class DuckSimulator {
     public static void main(String[] args) {
         DuckSimulator simulator = new DuckSimulator();
@@ -7,19 +9,21 @@ public class DuckSimulator {
         simulator.simulate();
     }
     private void simulate() {
-        Quackable mallardDuck = new MallardDuck();
-        Quackable redheadDuck = new RedheadDuck();
-        Quackable duckCall = new DuckCall();
-        Quackable rubberDuck = new RubberDuck();
+        Quackable mallardDuck = new QuackCounter(new MallardDuck());
+        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+        Quackable duckCall = new QuackCounter(new DuckCall());
+        Quackable rubberDuck = new QuackCounter(new RubberDuck());
         Quackable gooseDuck = new GooseAdapter(new Goose());
+        Quackable pigeonDuck = new PigeonAdapter(new Pigeon());
 
-        System.out.println("\nDuck Simulator: With Goose Adapter");
+        System.out.println("\nDuck Simulator: With Goose and Pigeon Adapter");
 
         simulate(mallardDuck);
         simulate(redheadDuck);
         simulate(duckCall);
         simulate(rubberDuck);
         simulate(gooseDuck);
+        simulate(pigeonDuck);
     }
     private void simulate(Quackable duck) {
         duck.quack();
---
+++ src/main/java/adapterPattern/PigeonAdapter.java
@@ -0,0 +1,13 @@
+package adapterPattern;
+
+public class PigeonAdapter implements Quackable {
+    private Pigeon pigeon;
+
+    public PigeonAdapter(Pigeon pigeon){
+        this.pigeon = pigeon;
+    }
+
+    public void quack() {
+        pigeon.coo();
+    }
+}
